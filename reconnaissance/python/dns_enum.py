68492fb986840b7cc78e2ac1e9922f69e2afff54
abraham lincoln <seven@localhost.localdomain>
2025-12-19 22:38:30 -0700

    added dns_enum.py

--- reconnaissance/python/dns_enum.py
+++ reconnaissance/python/dns_enum.py
@@ -7,21 +7,26 @@ print(dns)
 
 #ct logs
 print('-----------------SCANNING CT LOGS-----------------')
-res = requests.get('https://crt.sh/json', params={'q': url.split('://')[1]}, timeout=60)
-res.raise_for_status()
-data = res.json()
-domains = set()
-for entry in data:
-    name_value = entry.get('name_value', '')
-    for name in name_value.split('\n'):
-        domains.add(name.strip())
-for i in sorted(domains):
-    print(i)
+try:
+    res = requests.get('https://crt.sh/json', params={'q': url.split('://')[1]}, timeout=60)
+    res.raise_for_status()
+    data = res.json()
+    domains = set()
+    for entry in data:
+        name_value = entry.get('name_value', '')
+        for name in name_value.split('\n'):
+            domains.add(name.strip())
+    for i in sorted(domains):
+        print(i)
+except:
+    print('ct scan failed: skipping')
 #quieries
 print('-----------------RUNNING QUIERIES-----------------')
-os.system('dig ' + url)
-os.system('host ' + url)
-
+os.system('dig NS ' + url)
+os.system('dig A ' + url)
+os.system('dig MX ' + url)
+os.system(f'dig AXFR @{dns} {url}')
+os.system(f'dnsenum --dnsserver {dns} --enum {url}')
 #zone transfer attempt
 print('-----------------ZONE TRANSFER ATTEMPT-----------------')
 
@@ -35,3 +40,4 @@ print('-----------------REVERSE DNS-----------------')
 
 #txt records
 print('-----------------TXT RECORDS-----------------')
+os.system('dig TXT ' + url)
